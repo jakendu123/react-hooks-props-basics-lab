320b1d1158c831b506d0278ef4f390787123bf18
Fredrick Okoth <fredrick.okoth1@student.moringaschool.com>
2025-04-14 08:22:25 +0300

    props-lab

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+       <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
